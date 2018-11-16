296cc3c935cf8ea2d2d5ed1bc1ab2d2c1cc81d91
Michael Li <lvlichaelly@gmail.com>
2018-11-16 15:19:26 -0800

    added client folder

---
+++ client/src/api/httpClient.js
@@ -0,0 +1,27 @@
+//Create a Http Client using Axios. Further modifications in this layer can be done later like Authorization.
+import axios from 'axios' 
+
+const post = (url = '', data = '', config = {}) => {
+    return axios.post(url, data, config)
+}
+
+const get = (url) => {
+    return axios(url)
+}
+
+const put = (url = '', data = '', config = {}) => {
+    return axios.put(url, data, config)
+}
+
+const del = (url = '', config = {}) => {
+    return axios.delete(url, config)
+}
+
+const HttpClient = {
+    post,
+    get,
+    put,
+    delete: del
+}
+
+export {HttpClient}
